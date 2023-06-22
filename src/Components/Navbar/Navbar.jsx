5111f5af3c99edf5152de849696da38add721128
Hileene <linebeh@gmail.com>
2023-06-22 14:07:44 +0200

    Création du composant

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <div className="n-wrapper">
+        <div className="n-left">
+            <div className="n-name">Aline</div>
+            <span>toggle</span>
+        </div>
+        <div className="n-right">
+            <div className="n-list">
+                <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Experience</li>
+                    <li>Portfolio</li>
+                    <li>Testimonials</li>
+                </ul>
+            </div>
+            <button className="button n-button">
+                Contact us
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
